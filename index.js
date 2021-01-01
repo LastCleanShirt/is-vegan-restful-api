ae834e3b30e7d6e3d9f49bc0d225ef6dc5112241
arya <artris1357@gmail.com>
2021-01-01 21:06:28 +0700

    HTML

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ const data = require("./data.js");
 const readline = require("readline");
 const fs = require("fs");
 const app = express();
+const bodyParser = require("body-parser");
 
+app.use(express.static(__dirname + "/"));
 app.engine("html", require("ejs").renderFile);
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view options", {layout: false});
 app.use(express.static(__dirname + "/page/"));
+app.set("view engine", "ejs");
+app.set("views", __dirname)
+
+const IsVeganAPI = "Bruh"
 
 const readInterface = readline.createInterface({
 	input: fs.createReadStream("page/index.html"),
@@ -14,8 +21,8 @@ const readInterface = readline.createInterface({
 	console: false
 })
 
-app.get("/", (req, res) => {
-	res.render("/page/index.html");
+app.get("/", (req, res, next) => {
+	res.render("/page/index.html", {title: "IsVeganApi", message: "Bruh"});
 })
 
 app.get("/api/isVeganCheckIngredients", (req, res) => {
